269a92223f26b6fdc87fcaeed3bb4880698bfa8d
AMcBusiness <60987245+AMcBusiness@users.noreply.github.com>
2020-02-17 21:21:09 +0000

    Reupdate

--- includes/menu.inc.php
+++
@@ -1,4 +0,0 @@
-				<li class="<?php echo $page_id ==1 ? 'uk-active' : ''?>"><a href="/">Home</a></li>				
-				<li class="<?php echo $page_id ==2 ? 'uk-active' : ''?>"><a href="About.php">About</a></li>
-				<li class="<?php echo $page_id ==3 ? 'uk-active' : ''?>"><a href="Create.php">Create</a></li>
-				<li class="<?php echo $page_id ==4 ? 'uk-active' : ''?>"><a href="Library.php">Library</a></li>	
